167ee322902c58251364c571ff950f8d7dcce039
Gerry Boland <gerboland@gmail.com>
2023-02-01 11:00:18 +0000

    Add support for Outlier Detector

---
+++ mlapi/outlier.go
@@ -0,0 +1,102 @@
+package mlapi
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+)
+
+type OutlierAlgorithmConfig struct {
+	Epsilon float64 `json:"epsilon"`
+}
+
+type OutlierAlgorithm struct {
+	Name        string                  `json:"name"`
+	Sensitivity float64                 `json:"sensitivity"` // used by MAD
+	Config      *OutlierAlgorithmConfig `json:"config"`      // used by DBSCAN
+}
+
+// OutlierDetector defines an outlier detector instance
+type OutlierDetector struct {
+	ID string `json:"id,omitempty"`
+	// Name is a human readable name for the outlier detector.
+	Name string `json:"name"`
+	// Metric is the metric name used to query the outlier detector. Must match Prometheus
+	// naming requirements:
+	// https://prometheus.io/docs/concepts/data_model/#metric-names-and-labels.
+	Metric      string `json:"metric"`
+	Description string `json:"description"`
+
+	// GrafanaURL is the full URL to the Grafana instance. For example,
+	// https://myinstance.grafana.net/.
+	GrafanaURL string `json:"grafanaUrl"`
+	// DatasourceID is the numeric ID of the datasource to query when training
+	// data.
+	DatasourceID uint `json:"datasourceId"`
+	// DatasourceUID is the string UID of the datasource to query when training
+	// data.
+	DatasourceUID  string                 `json:"datasourceUid"`
+	DatasourceType string                 `json:"datasourceType"`
+	QueryParams    map[string]interface{} `json:"queryParams"`
+	// Interval is the data resolution in seconds.
+	Interval uint `json:"interval"`
+
+	// Algorithm specifies the algorithm to use and its configuration. See
+	// https://grafana.com/docs/grafana-cloud/machine-learning/outlier-detection/ for the
+	// options.
+	Algorithm OutlierAlgorithm `json:"algorithm"`
+}
+
+type outlierDetectorResponseWrapper struct {
+	Status string          `json:"status"`
+	Data   OutlierDetector `json:"data"`
+	Error  string          `json:"error"`
+}
+
+// NewOutlierDetector creates an outlier detector.
+func (c *Client) NewOutlierDetector(ctx context.Context, outlier OutlierDetector) (OutlierDetector, error) {
+	data, err := json.Marshal(outlier)
+	if err != nil {
+		return OutlierDetector{}, err
+	}
+
+	result := outlierDetectorResponseWrapper{}
+	err = c.request(ctx, "POST", "/manage/api/v1/outliers", nil, bytes.NewBuffer(data), &result)
+	if err != nil {
+		return OutlierDetector{}, err
+	}
+	return result.Data, nil
+}
+
+// OutlierDetector fetches an existing outlier detector.
+func (c *Client) OutlierDetector(ctx context.Context, id string) (OutlierDetector, error) {
+	result := outlierDetectorResponseWrapper{}
+	err := c.request(ctx, "GET", "/manage/api/v1/outliers/"+id, nil, nil, &result)
+	if err != nil {
+		return OutlierDetector{}, err
+	}
+	return result.Data, nil
+}
+
+// UpdateOutlierDetector updates a outlier detector. A new training will be scheduled as part of updating.
+func (c *Client) UpdateOutlierDetector(ctx context.Context, outlier OutlierDetector) (OutlierDetector, error) {
+	id := outlier.ID
+	// Clear the ID before sending otherwise validation fails.
+	outlier.ID = ""
+	data, err := json.Marshal(outlier)
+	if err != nil {
+		return OutlierDetector{}, err
+	}
+
+	result := outlierDetectorResponseWrapper{}
+	err = c.request(ctx, "POST", "/manage/api/v1/outliers/"+id, nil, bytes.NewBuffer(data), &result)
+	if err != nil {
+		return OutlierDetector{}, err
+	}
+	return result.Data, nil
+}
+
+// DeleteOutlierDetector deletes outlier detector.
+func (c *Client) DeleteOutlierDetector(ctx context.Context, id string) error {
+	return c.request(ctx, "DELETE", "/manage/api/v1/outliers/"+id, nil, nil, nil)
+}
---
+++ mlapi/outlier_test.go
@@ -0,0 +1,142 @@
+package mlapi
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewOutlierDetector(t *testing.T) {
+	id := "8b154ff8-3d64-4b79-8b26-02b4baeb44e4"
+	outlier := OutlierDetector{}
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		if r.URL.Path != "/manage/api/v1/outliers" {
+			http.Error(w, "not found", http.StatusNotFound)
+			return
+		}
+		parsedOutlierDetector := OutlierDetector{}
+		dec := json.NewDecoder(r.Body)
+		err := dec.Decode(&parsedOutlierDetector)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		assert.Equal(t, outlier, parsedOutlierDetector)
+		parsedOutlierDetector.ID = id
+		enc := json.NewEncoder(w)
+		_ = enc.Encode(outlierDetectorResponseWrapper{Data: parsedOutlierDetector})
+	}))
+	defer s.Close()
+
+	c, err := New(s.URL, Config{})
+	require.NoError(t, err)
+	ctx := context.Background()
+
+	returnedOutlierDetector, err := c.NewOutlierDetector(ctx, outlier)
+	require.NoError(t, err)
+	assert.Equal(t, id, returnedOutlierDetector.ID)
+}
+
+func TestOutlierDetector(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		if r.URL.Path != "/manage/api/v1/outliers/8b154ff8-3d64-4b79-8b26-02b4baeb44e4" {
+			http.Error(w, "not found", http.StatusNotFound)
+			return
+		}
+		_, err := w.Write([]byte(
+			`{"status":"success","data":{"id":"8b154ff8-3d64-4b79-8b26-02b4baeb44e4","created":"2022-01-05T15:48:48.647Z","modified":"2022-01-05T15:48:48.647Z","createdBy":"a_user","modifiedBy":null,"name":"Test OutlierDetector","metric":"test_outlier","description":"","grafanaUrl":"http://localhost:3000/","grafanaApiKey":"\u003credacted\u003e","datasourceId":10,"datasourceUid":"abcd1234","datasourceType":"prometheus","queryParams":{"exemplar":true,"expr":"sum(up)","interval":"","legendFormat":"","refId":"A"},"interval":300,"algorithm":{"name":"dbscan","config":{"eps":0.5}}}}`,
+		))
+		require.NoError(t, err)
+	}))
+	defer s.Close()
+
+	c, err := New(s.URL, Config{})
+	require.NoError(t, err)
+	ctx := context.Background()
+
+	id := "8b154ff8-3d64-4b79-8b26-02b4baeb44e4"
+	outlier, err := c.OutlierDetector(ctx, id)
+	require.NoError(t, err)
+	assert.Equal(t, id, outlier.ID)
+	assert.Equal(t, "Test OutlierDetector", outlier.Name)
+	assert.Equal(t, "test_outlier", outlier.Metric)
+	assert.NotEmpty(t, outlier.QueryParams)
+	assert.NotEmpty(t, outlier.Algorithm)
+}
+
+func TestUpdateOutlierDetector(t *testing.T) {
+	id := "8b154ff8-3d64-4b79-8b26-02b4baeb44e4"
+	outlier := OutlierDetector{
+		ID: id,
+	}
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		if r.URL.Path != "/manage/api/v1/outliers/"+id {
+			http.Error(w, "not found", http.StatusNotFound)
+			return
+		}
+		parsedOutlierDetector := OutlierDetector{}
+		dec := json.NewDecoder(r.Body)
+		err := dec.Decode(&parsedOutlierDetector)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if parsedOutlierDetector.ID != "" {
+			http.Error(w, "id should be empty when updating", http.StatusBadRequest)
+			return
+		}
+		parsedOutlierDetector.ID = id
+		assert.Equal(t, outlier, parsedOutlierDetector)
+		enc := json.NewEncoder(w)
+		_ = enc.Encode(outlierDetectorResponseWrapper{Data: parsedOutlierDetector})
+	}))
+	defer s.Close()
+
+	c, err := New(s.URL, Config{})
+	require.NoError(t, err)
+	ctx := context.Background()
+
+	returnedOutlierDetector, err := c.UpdateOutlierDetector(ctx, outlier)
+	require.NoError(t, err)
+	assert.Equal(t, outlier, returnedOutlierDetector)
+}
+
+func TestDeleteOutlierDetector(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "DELETE" {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		if r.URL.Path != "/manage/api/v1/outliers/8b154ff8-3d64-4b79-8b26-02b4baeb44e4" {
+			http.Error(w, "not found", http.StatusNotFound)
+			return
+		}
+		_, err := w.Write([]byte("successfully deleted"))
+		require.NoError(t, err)
+	}))
+	defer s.Close()
+
+	c, err := New(s.URL, Config{})
+	require.NoError(t, err)
+	ctx := context.Background()
+
+	err = c.DeleteOutlierDetector(ctx, "8b154ff8-3d64-4b79-8b26-02b4baeb44e4")
+	require.NoError(t, err)
+}
